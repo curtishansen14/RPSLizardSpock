a461a3a43f8a549beca5e4e4f96879e0e0e5a046
Curtis Hansen <curtishansen14@gmail.com>
2018-02-02 09:15:02 -0600

    added classes, wrote out UI

---
+++ RockPaperScissorsLizardSpock/Game.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissorsLizardSpock
+{
+    class Game
+    {
+        Player playerOne;
+        Player playerTwo;
+
+        public void playGame()
+        {
+
+            switch (getNumberofHumans())
+            {
+                case "1":
+                    //functions for player vs computer
+                    break;
+                case "2":
+                    //functions for player vs player
+                    break;
+                default:
+                    UI.PleaseTryAgain();
+                    UI.askNumberofHumans();
+                    playGame();
+                    //text function that says try again.
+                    break;
+            }
+        }
+
+        public string getNumberofHumans()
+        {
+            string humans;
+            UI.askNumberofHumans();
+            humans = Console.ReadLine();
+            return humans;
+        }
+    }
+}
